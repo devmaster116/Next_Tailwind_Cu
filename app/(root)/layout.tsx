d87cfc2163f16939a915623b6ebedf63343ea2d3
devmaster116 <devmaster116@gmail.com>
2024-06-03 13:00:25 +1000

    Further file restructure

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
-import "./globals.css";
+import "../../app/globals.css";
 import type { Metadata } from "next";
-import LogoutButton from "./components/Auth/LogoutButton";
+import LogoutButton from "../components/Auth/LogoutButton";
 
 export const metadata: Metadata = {
   title: "Customer admin webapp",
--- app/(root)/reports-dashboard/components/Reports.tsx
+++ app/(root)/reports-dashboard/components/Reports.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import withAuth from "../../components/Auth/withAuth";
+import withAuth from "../../../components/Auth/withAuth";
 import {
   functions,
   httpsCallable,
