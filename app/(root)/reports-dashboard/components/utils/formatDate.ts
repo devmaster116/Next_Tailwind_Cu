377e3a106e6540cc5c30794ceaad02e5fca3e545
devmaster116 <devmaster116@gmail.com>
2024-06-15 14:06:15 +1000

    Add format date utility

--- app/(root)/reports-dashboard/components/utils/formatDate.ts
+++ app/(root)/reports-dashboard/components/utils/formatDate.ts
@@ -13,3 +13,37 @@ export const getTopFive = (items: KitchenItem[]): KitchenItem[] => {
   const sortedItems = items.sort((a, b) => b.item_count - a.item_count);
   return sortedItems.slice(0, 5);
 };
+
+export const getYesterdayDate = (): Date => {
+  const today = new Date();
+  today.setDate(today.getDate() - 1);
+  return today;
+};
+
+export function getCurrentWeekRange(): { startDate: Date; endDate: Date } {
+  const today = new Date();
+  const day = today.getDay();
+  const diffToMonday = (day === 0 ? -6 : 1) - day;
+  const diffToSunday = day === 0 ? 0 : 7 - day;
+
+  const startDate = new Date(today);
+  startDate.setDate(today.getDate() + diffToMonday);
+
+  const endDate = new Date(today);
+  endDate.setDate(today.getDate() + diffToSunday);
+
+  return { startDate, endDate };
+}
+
+export function getCurrentMonthRange(): {
+  startMonthDate: Date;
+  endMonthDate: Date;
+} {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth();
+  const startMonthDate = new Date(year, month, 1);
+  const endMonthDate = new Date(year, month + 1, 0);
+
+  return { startMonthDate, endMonthDate };
+}
