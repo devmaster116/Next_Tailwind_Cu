6d67464cc754717bc7fc850248509cbaa79b3dd1
devmaster116 <devmaster116@gmail.com>
2024-06-22 12:00:48 +1000

    Add empty states for sales data

--- app/(root)/reports-dashboard/components/SalesData.tsx
+++ app/(root)/reports-dashboard/components/SalesData.tsx
@@ -15,13 +15,25 @@ const SalesData = ({
   isDollarAmount,
   loading,
 }: SalesDataProps) => {
+  const getDisplayAmount = (amount: number | undefined): number => {
+    return isNaN(amount as number) || amount === undefined ? 0 : amount;
+  };
+
   return (
     <>
       <div className={styles.card}>
         <div className={styles.cardInfo}>
           <h3>{title}</h3>
           {loading && <SalesDataSkeleton />}
-          {!loading && <h2>{isDollarAmount ? `$${amount}` : `${amount}`}</h2>}
+          {!loading && (
+            <h2>
+              {isDollarAmount
+                ? !amount
+                  ? `$${getDisplayAmount(amount)}.00`
+                  : `$${getDisplayAmount(amount)}`
+                : `${getDisplayAmount(amount)}`}
+            </h2>
+          )}
         </div>
       </div>
     </>
