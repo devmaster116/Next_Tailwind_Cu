9a25af4d7f6325a2a9b195f83271265a3a4b0805
devmaster116 <devmaster116@gmail.com>
2024-07-11 20:43:57 +1000

    Add Averages table

--- app/src/types.ts
+++ app/src/types.ts
@@ -21,15 +21,22 @@ export interface Dishes extends KitchenItem {
 }
 
 export interface OrdersResponse {
+  dine_in_order_net_avg: number;
+  online_order_net_avg: number;
+  take_away_order_net_avg: number;
   total_card_orders: number;
+  total_card_refunded_sum: number;
   total_card_sum: number;
   total_card_surcharge: number;
   total_card_tip: number;
   total_cash_orders: number;
+  total_cash_refunded_sum: number;
   total_cash_sum: number;
+  total_completed_orders: number;
   total_dine_in_orders: number;
   total_holiday_surcharge: number;
   total_net_sales: number;
+  total_online_orders: number;
   total_orders: number;
   total_refunded_orders: number;
   total_refunded_sum: number;
