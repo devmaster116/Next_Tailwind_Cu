9333f49f0a83a6dc7ee68b4c16bb8b218396318a
devmaster116 <devmaster116@gmail.com>
2024-07-12 12:29:55 +1000

    Using FIREBASE_CONFIG environment variable from CloudBuild to set env config

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,24 +1,36 @@
-import { getApp, getApps, initializeApp } from "firebase/app";
+import { FirebaseOptions, getApp, getApps, initializeApp } from "firebase/app";
 import "firebase/auth";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getFunctions, httpsCallable } from "firebase/functions";
 
-const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  databaseURL: process.env.databaseURL,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId,
-};
+// Parse FIREBASE_CONFIG from environment variable
+const firebaseConfigString = process.env.FIREBASE_CONFIG ? process.env.FIREBASE_CONFIG  : process.env.LOCAL_FIREBASE_CONFIG;
 
-// const app = initializeApp(firebaseConfig);
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const auth = getAuth(app);
-const functions = getFunctions(app, "australia-southeast1");
-const db = getFirestore(app);
+let firebaseConfig: FirebaseOptions | undefined;
+
+if (firebaseConfigString) {
+  try {
+    firebaseConfig = firebaseConfigString as FirebaseOptions;
+  } catch (error) {
+    console.error("Error parsing Firebase config:", error);
+  }
+}
+
+let app;
+let auth;
+let functions;
+let db;
+
+// Check if firebaseConfig is valid before initializing Firebase app
+if (firebaseConfig) {
+  app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+  auth = getAuth(app);
+  functions = getFunctions(app, "australia-southeast1");
+  db = getFirestore(app);
+} else {
+  console.error("Firebase config is not valid.");
+  throw Error("Config is not valid.");
+}
 
 export { auth, functions, db, httpsCallable };
