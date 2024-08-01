a4366a4c6b115ec9363a815d98ead567baf22f2d
devmaster116 <devmaster116@gmail.com>
2024-08-01 23:01:53 +1000

    Calculation fixes and modifying useFetchReports to call the new function

--- app/(root)/sales-summary/SalesSummary.tsx
+++ app/(root)/sales-summary/SalesSummary.tsx
@@ -11,6 +11,7 @@ import useWindowSize from "@/app/hooks/useWindowSize";
 import withAuth from "@/app/components/Auth/withAuth";
 import { useKitchen } from "../../context/KitchenContext";
 import useFetchReports from "@/app/hooks/useFetchReports";
+import { dishDetailsByOrderTypeParser } from "./utils/dishDetailsByOrderTypeParser";
 
 const SalesSummary = () => {
   const [reportEndDate, setReportEndDate] = useState(new Date());
@@ -28,6 +29,7 @@ const SalesSummary = () => {
     customDate,
     setCustomDate,
     ordersData,
+    dishByOrderType,
     overviewReportFunctionError,
   } = useFetchReports(
     kitchenId,
@@ -37,8 +39,10 @@ const SalesSummary = () => {
     {
       fetchAdvancedReports: false,
       fetchOverviewReports: true,
+      fetchDishesCountByOrderType: true
     }
   );
+
 
   const calculatePercentage = (
     numerator: number,
@@ -57,17 +61,30 @@ const SalesSummary = () => {
   let take_away_order_net_avg = 0;
   let total_card_orders = 0;
   let total_card_sum = 0;
+  let total_take_away_sum = 0;
+  let total_dine_in_sum = 0;
   let total_card_surcharge = 0;
   let total_card_tip = 0;
   let total_cash_orders = 0;
   let total_cash_sum = 0;
   let total_dine_in_orders = 0;
+  let total_holiday_surcharge = 0;
   let total_net_sales = 0;
   let total_online_orders = 0;
   let total_orders = 0;
   let total_refunded_sum = 0;
   let total_revenue = 0;
   let total_take_away_orders = 0;
+
+  // Extract details dynamically
+const takeAwayItemDetails = dishDetailsByOrderTypeParser(dishByOrderType, "Take Away");
+const dineInItemDetails = dishDetailsByOrderTypeParser(dishByOrderType, "Dine In");
+
+const takeAwayItemCount = takeAwayItemDetails ? takeAwayItemDetails.itemCount : 0;
+const takeAwayItemTotal = takeAwayItemDetails ? takeAwayItemDetails.totalPrice : 0;
+const dineInItemCount = dineInItemDetails ? dineInItemDetails.itemCount : 0;
+const dineInItemTotal = dineInItemDetails ? dineInItemDetails.totalPrice : 0;
+
 
   if (ordersData?.length > 0) {
     ({
@@ -76,11 +93,14 @@ const SalesSummary = () => {
       take_away_order_net_avg,
       total_card_orders,
       total_card_sum,
+      total_take_away_sum,
+      total_dine_in_sum,
       total_card_surcharge,
       total_card_tip,
       total_cash_orders,
       total_cash_sum,
       total_dine_in_orders,
+      total_holiday_surcharge,
       total_net_sales,
       total_online_orders,
       total_orders,
@@ -172,8 +192,8 @@ const SalesSummary = () => {
                 refund: total_refunded_sum || 0,
               },
               {
-                title: "GST (10%)",
-                refund: total_net_sales / 11 || 0,
+                title: "GST",
+                refund: (total_net_sales) * 0.1 || 0,
               },
             ]}
             loading={loading}
@@ -186,8 +206,8 @@ const SalesSummary = () => {
             secondColumnSymbol="$"
             dataObj={[
               {
-                title: "Includes PH Surcharge 15%",
-                tips: total_card_tip || 0,
+                title: "Includes PH Surcharge",
+                tips: total_holiday_surcharge || 0,
               },
             ]}
             loading={loading}
@@ -221,7 +241,7 @@ const SalesSummary = () => {
                   total_take_away_orders + total_dine_in_orders || 0,
                   1
                 )}%)`,
-                net: total_net_sales / total_take_away_orders || 0,
+                net: total_take_away_sum ? total_take_away_sum/1.1 : 0,
               },
               {
                 title: "Dine In",
@@ -230,7 +250,7 @@ const SalesSummary = () => {
                   total_take_away_orders + total_dine_in_orders || 0,
                   1
                 )}%)`,
-                net: total_net_sales / total_dine_in_orders || 0,
+                net: total_dine_in_sum? total_dine_in_sum/1.1 : 0,
               },
             ]}
             loading={loading}
