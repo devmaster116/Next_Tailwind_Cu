1da19f822bc6f2aaba1e22cc6c95b7b86e71127a
devmaster116 <devmaster116@gmail.com>
2024-08-19 04:25:39 -0400

    fix style

--- app/(root)/staff-members/components/staff-view/index.tsx
+++ app/(root)/staff-members/components/staff-view/index.tsx
@@ -38,36 +38,37 @@ const StaffView = (props: Props) => {
         
           </div>
       <div className='p-4'>
-        <div className='shadow border-lg border-gray-200 rounded'> 
+        <div className='shadow border-lg border-gray-200 rounded-lg'> 
           <div className='flex items-center justify-center border-b border-gray-200 py-4'>
               <div className='flex flex-col items-center gap-2'>
                   <div className='w-16 h-16 rounded-full bg-gray-200 flex items-center justify-center'>
                       <span className='text-2xl font-medium text-gray-600'>GC</span>
                   </div>
-                  <p className="font-semibold  text-gray-800 text-[14px] leading-[20px] md:text-[16px] md:leading-[24px] text-purple-700">Update Photo</p>
+                  <p className="font-semibold  text-gray-800 text-[14px] leading-[20px] sm:text-[16px] sm:leading-[24px] text-purple-700">Update Photo</p>
               </div>
           </div>
-          <div className='flex flex-col'>
+          <div className={twMerge('flex flex-col')} style={{ boxShadow: '0 1px 2px 0 rgba(16, 24, 40, 0.05)' }}>
+          {/* <div className= {twMerge('flex flex-col', styles['box-shadow: 0 1px 2px 0 rgba(16, 24, 40, 0.05)'])}> */}
             <div className='p-4'>
                 <Paragraph 
                   title='Personal Details'
                   content='Edit'
                   classOverride={{
-                    container: 'flex-row justify-between mt-4 mb-2',
-                    title: 'font-bold text-[16px] leading-[24px] md:text-[18px] md:leading-[28px]',
-                    content: 'font-semibold text-purple-700 text-[16px] leading-[24px] md:text-[18px] md:leading-[28px]'
+                    container: 'flex-row justify-between mb-2',
+                    title: 'font-bold text-[16px] leading-[24px] sm:text-[18px] sm:leading-[28px]',
+                    content: 'font-semibold text-purple-700 text-[16px] leading-[24px] sm:text-[18px] sm:leading-[28px]'
                   }}
                   onClick={() => console.log('clicked')}
                 />
 
-                <div className="flex py-4">
+                <div className="flex py-[10px] sm:py-3">
                     <div className="flex-1 w-32">
                       <Paragraph 
                         title='First Name'
                         content={data.firstName}
                         classOverride={{
-                             title: 'text-gray-800 font-semibold text-[14px] leading-[20px] md:text-[16px] md:leading-[24px]',
-                             content: 'font-normal  text-[14px] leading-[20px] md:text-[16px] md:leading-[24px]'
+                             title: 'text-gray-800 font-semibold text-[14px] leading-[20px] sm:text-[16px] sm:leading-[24px]',
+                             content: 'font-normal  text-[14px] leading-[20px] sm:text-[16px] sm:leading-[24px]'
                         }}
                       />
                     </div>
@@ -76,21 +77,21 @@ const StaffView = (props: Props) => {
                           title='Last Name'
                           content={data.lastName}
                           classOverride={{
-                           title: 'text-gray-800 font-semibold text-[14px] leading-[20px] md:text-[16px] md:leading-[24px]',
-                           content: 'font-normal  text-[16px] leading-[24px] md:text-[18px] md:leading-[28px]'
+                           title: 'text-gray-800 font-semibold text-[14px] leading-[20px] sm:text-[16px] sm:leading-[24px]',
+                           content: 'font-normal  text-[16px] leading-[24px] sm:text-[18px] sm:leading-[28px]'
                           }}
                         />
 
                     </div>
                 </div>
-                <div className="flex py-4">
+                <div className="flex py-[10px] sm:py-3">
                     <div className="flex-1 w-32">
                       <Paragraph 
                         title='Nick Name'
                         content={data.displayName}
                         classOverride={{
-                        title: 'text-gray-800 font-semibold text-[14px] leading-[20px] md:text-[16px] md:leading-[24px]',
-                        content: 'font-normal  text-[16px] leading-[24px] md:text-[18px] md:leading-[28px]'
+                        title: 'text-gray-800 font-semibold text-[14px] leading-[20px] sm:text-[16px] sm:leading-[24px]',
+                        content: 'font-normal  text-[16px] leading-[24px] sm:text-[18px] sm:leading-[28px]'
                         }}
                       />
                     </div>
@@ -99,21 +100,21 @@ const StaffView = (props: Props) => {
                           title='Mobile'
                           content={data?.phoneNumber}
                           classOverride={{
-                           title: 'text-gray-800 font-semibold text-[14px] leading-[20px] md:text-[16px] md:leading-[24px]',
-                           content: 'font-normal  text-[16px] leading-[24px] md:text-[18px] md:leading-[28px]'
+                           title: 'text-gray-800 font-semibold text-[14px] leading-[20px] sm:text-[16px] sm:leading-[24px]',
+                           content: 'font-normal  text-[16px] leading-[24px] sm:text-[18px] sm:leading-[28px]'
                           }}
                         />
 
                     </div>
                 </div>
               
-                <div className='flex py-4 pt-3'>
+                <div className='flex py-[10px] sm:py-3'>
                       <Paragraph 
                           title='Email Address'
                           content={'Alfonso.Cassano@gmail.com'}
                           classOverride={{
-                            title: 'text-gray-800 font-semibold text-[14px] leading-[20px] md:text-[16px] md:leading-[24px]',
-                            content: 'font-normal  text-[16px] leading-[24px] md:text-[18px] md:leading-[28px]'
+                            title: 'text-gray-800 font-semibold text-[14px] leading-[20px] sm:text-[16px] sm:leading-[24px]',
+                            content: 'font-normal  text-[16px] leading-[24px] sm:text-[18px] sm:leading-[28px]'
                           }}
                         />
                 </div>
@@ -125,8 +126,8 @@ const StaffView = (props: Props) => {
                 content='Edit'
                 classOverride={{
                   container: 'flex-row justify-between',
-                  title: 'font-bold text-[16px] leading-[24px] md:text-[18px] md:leading-[28px]',
-                  content: 'font-semibold text-purple-700 text-[16px] leading-[24px] md:text-[18px] md:leading-[28px]'
+                  title: 'font-bold text-[16px] leading-[24px] sm:text-[18px] sm:leading-[28px]',
+                  content: 'font-semibold text-purple-700 text-[16px] leading-[24px] sm:text-[18px] sm:leading-[28px]'
                 }}
                 onClick={() => console.log('clicked')}
               />
@@ -144,20 +145,30 @@ const StaffView = (props: Props) => {
                   content='Generate'
                   classOverride={{
                     container: 'flex-row justify-between',
-                    title: 'font-bold text-[16px] leading-[24px] md:text-[18px] md:leading-[28px]',
-                    content: 'font-semibold text-purple-700 text-[16px] leading-[24px] md:text-[18px] md:leading-[28px]'
+                    title: 'font-bold text-[16px] leading-[24px] sm:text-[18px] sm:leading-[28px]',
+                    content: 'font-semibold text-purple-700 text-[16px] leading-[24px] sm:text-[18px] sm:leading-[28px]'
                   }}
                   onClick={() => console.log('clicked')}
                 />
                 <div className='flex flex-row  ' >
-                <p className=' pt-1 pr-3 pb-1 pl-3 font-base  font-medium   rounded-2xl bg-purple-50 text-purple-700'>{data.passcode?data.passcode:'No Code Set'}</p>
+                {data.passcode&&(
+                    <div className='flex flex-row'>
+                      <div className='flex flex-direction '>
+                          <p className='font-bold  text-[18px] leading-[28px] sm:text-[20px] sm:leading-[28px] text-gray-800 gap-1'>****</p>
+                      </div>
+                      <div className='flex flex-direction'>
+                          <p className=' flex items-center gap-1 font-bold text-sm leading-5 sm:text-base sm:leading-6 text-purple-700'>Show Code</p>
+                      </div>
+                    </div>
+                )}
+                {!data.passcode&&(<p className=' pt-1 pr-3 pb-1 pl-3 font-base  font-medium   rounded-2xl bg-purple-50 text-purple-700'>No Code Set</p>)}
                 </div>
             </div>
           </div>
         </div>
       </div>
       <div className='flex items-center justify-center pb-4'>
-        <button className="hover:text-white text-rose-700 bg-rose-50 font-base pt-[10px] pr-[18px] pb-[10px] pl-[18px] rounded-lg hover:bg-rose-600" onClick={props.onDeleteModalOpen}>Delete Staff Member</button>
+        <button className="hover:text-white text-rose-700 bg-rose-50 text-[14px] leading-[20px] sm:text-[16px] sm:leading-[24px] pt-[10px] pr-[18px] pb-[10px] pl-[18px] rounded-lg hover:bg-rose-600" onClick={props.onDeleteModalOpen}>Delete Staff Member</button>
       </div>
       </div>
       )}
